fafecc2e1b7efadf636c96fc7ed9127f95136f77
dansch1982 <dansch1982@gmail.com>
2022-03-17 14:05:26 +0100

    sum

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ class Weather {
         const API_KEY = 'fd3ccc871f6c471a91d78a0f64734999'
         const url = new URL(`https://api.opencagedata.com/geocode/v1/json?q=${this.location.lat}+${this.location.lon}&key=${API_KEY}`)
         const data = await (await fetch(url)).json();
-        console.log(data)
         return data.results[0].formatted
     }
     createElement(parent) {
@@ -66,8 +65,7 @@ class Weather {
 
         return section;
     }
-    async start() {
-        this.name = await this.getLocationName()
+    async start() {        this.name = await this.getLocationName()
         this.update();
         this.interval = setInterval(() => {
             this.update();
@@ -81,12 +79,11 @@ class Weather {
         if (!document.getElementById('weatherAppCSS')) {
             const style = document.createElement('style')
             style.id = "weatherAppCSS"
-            style.innerHTML = '*{padding:0;margin:0;box-sizing:border-box}body{margin:auto;width:90vw;height:70vh}.create{background-color:#f0f8ff;display:flex;flex-direction:column;gap:.5em;padding:.5rem;font-family:"Segoe UI",Tahoma,Geneva,Verdana,sans-serif}.create label{display:flex;align-items:center;font-size:1.5rem}.create input{padding:.5rem;border:2px solid #000;margin-right:.5rem;border-radius:5px}.create button{width:-webkit-fit-content;width:-moz-fit-content;width:fit-content;min-width:4rem;padding:.5rem;border-radius:0;border:2px solid #000;background-color:unset;border-radius:5px}.weatherContainer{display:grid;width:100%;height:100%;overflow:hidden}[id^=weather_]{background-color:#f5f5f5;box-sizing:border-box;display:flex;flex-direction:column;justify-content:center;align-items:center;width:100%;height:100%;padding:.5em;gap:.5em}[id^=weather_] *{padding:0;margin:0;box-sizing:border-box}[id^=weather_] [id^=weather_][id$=_temp]{display:flex;justify-content:center;align-items:center;text-align:center;height:-webkit-fit-content;height:-moz-fit-content;height:fit-content;width:-webkit-fit-content;width:-moz-fit-content;width:fit-content;white-space:pre}[id^=weather_] [id^=weather_][id$=_symbol]{position:relative;justify-content:center;align-items:center;width:100%;height:100%}[id^=weather_] [id^=weather_][id$=_symbol] svg,[id^=weather_] [id^=weather_][id$=_symbol] img{position:absolute;top:5%;left:5%;width:90%;height:90%}/*# sourceMappingURL=style.css.map */'
+            style.innerHTML = '[id^=weather_]{background-color:#f5f5f5;box-sizing:border-box;display:flex;flex-direction:column;justify-content:center;align-items:center;width:100%;height:100%;padding:.5em;gap:.5em}[id^=weather_] *{padding:0;margin:0;box-sizing:border-box}[id^=weather_] [id^=weather_][id$=_temp]{display:flex;justify-content:center;align-items:center;text-align:center;height:-webkit-fit-content;height:-moz-fit-content;height:fit-content;width:-webkit-fit-content;width:-moz-fit-content;width:fit-content;white-space:pre}[id^=weather_] [id^=weather_][id$=_symbol]{position:relative;justify-content:center;align-items:center;width:100%;height:100%}[id^=weather_] [id^=weather_][id$=_symbol] svg,[id^=weather_] [id^=weather_][id$=_symbol] img{position:absolute;top:5%;left:5%;width:90%;height:90%}/*# sourceMappingURL=weather.css.map */'
             document.head.appendChild(style)
         }
     }
 }
-const parent = document.querySelector(".weatherContainer")
 
 const create = document.getElementById('create')
 create.addEventListener('click', () => {
@@ -94,6 +91,17 @@ create.addEventListener('click', () => {
     const lat = parseFloat(latitude.value)
     const longitude = document.getElementById('longitude')
     const lon = parseFloat(longitude.value)
+    const parent = document.querySelector(".weatherContainer")
     const weather = new Weather(lat, lon, 1, parent);
     weather.start();
 })
+
+const observer = new MutationObserver((mutationsList, observer) => {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            weatherContainer.style.gridTemplateColumns = `repeat(${weatherContainer.children.length < 4 ? weatherContainer.children.length : 4}, 1fr)`
+        }
+    }
+});
+const weatherContainer = document.querySelector('.weatherContainer')
+observer.observe(weatherContainer, { childList: true, subtree: false });
